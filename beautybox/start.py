72bb3b68015d2476c2308f9fc5c5c3c112214e29
roman <romanshashkin@mail.ru>
2021-01-28 14:47:09 +0400

    add reboot parser in while true cycle

--- beautybox/start.py
+++ beautybox/start.py
@@ -1,5 +1,6 @@
 from beautybox.avito_ads_parser import main as ads_parser
 from beautybox.avito_urls_parser import main as urls_parser
+from time import sleep
 
 
 def start_ads_parser():
@@ -7,5 +8,6 @@ def start_ads_parser():
         try:
             ads_parser()
             urls_parser()
-        except:
-            pass
+        except Exception:
+            print(Exception)
+            sleep(30)
